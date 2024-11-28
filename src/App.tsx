1f9a108add838f5d90746c27b21fe4352b10dbc5
Marcos Daniel Romero Garcia <Marck Romero>
2024-11-28 03:03:36 -0500

    Portfolio en construccion - Tiene para rato

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { NavPort } from "./components/header/NavPort"
+
+
 export const App = () => {
+  
   return (
-    <div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus consectetur, sit debitis reiciendis tempore fugiat! Nulla, maxime explicabo. Veniam dolores perspiciatis officia delectus laborum. Sapiente voluptatibus eius modi repudiandae fugiat. <span>NEGRO</span> </p>
-    </div>
+    <>
+      <NavPort />
+    </>
   )
 }
---
+++ src/components/header/NavPort.tsx
@@ -0,0 +1,16 @@
+export const NavPort = () => {
+  return (
+    <section className="portNav contenedor">
+        <p className="portNav__name">
+            Marcos Romero
+        </p>
+        <p className="portNav__role">
+            Desarrollador Frontend
+        </p>
+
+        <p className="portNav__textBody">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut aperiam, voluptatibus molestias 
+        </p>
+    </section>
+  )
+}
