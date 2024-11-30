b2cd1de99c9b52cea92b65f32a532e4cb497d73b
Marcos Daniel Romero Garcia <Marck Romero>
2024-11-30 02:27:10 -0500

    Se agrego mapeos CSS

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    devSourcemap: true, // Habilita source maps en desarrollo
+  },
+  build: {
+    sourcemap: true, // Habilita source maps en producción
+  },
 })
